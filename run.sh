af3e78adcb464b689e9cb8b6b9e76142699b1e2e
Hamidreza Sanaee <shrsanaee@gmail.com>
2021-05-09 00:44:27 +0430

    Fix run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,9 @@ source env/bin/activate
 cd src/crawler/
 scrapy crawl r6 -O ../../data/r6.json
 scrapy crawl csgo -O ../../data/csgo.json
-cd ../../
-python src/pre_processors/clean_data.py 
-python src/pre_processors/sent_tokenizer.py
-python src/pre_processors/word_tokenizer.py
-python src/stats/exract_stats.py 
+cd ../../src/pre_processors
+python clean_data.py 
+python sent_tokenizer.py
+python word_tokenizer.py
+cd ../stats
+python exract_stats.py 
