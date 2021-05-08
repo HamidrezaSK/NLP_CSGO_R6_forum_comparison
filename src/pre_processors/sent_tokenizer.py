397dfcd76dd3c8a2f63f4f5a5745959cf067db69
Hamidreza Sanaee <shrsanaee@gmail.com>
2021-05-09 00:02:43 +0430

    Complete setn_tokenizer

--- src/pre_processors/sent_tokenizer.py
+++ src/pre_processors/sent_tokenizer.py
@@ -1,9 +1,8 @@
 from nltk.tokenize import sent_tokenize
 import json
 import re
-import emoji
 
-file = open("cleaned_csgo.json",'r')
+file = open("../../data/cleaned_csgo.json",'r')
 
 data = json.load(file)
 
@@ -11,6 +10,19 @@ sentences = []
 for line in data:
     comment = line["csgo"]
     sentences.extend(sent_tokenize(comment))
-with open('sentences.json', 'w') as outfile:
+with open('../../data/sentences_csgo.json', 'w') as outfile:
+    json.dump(sentences, outfile)
+file.close()
+
+
+file = open("../../data/cleaned_r6.json",'r')
+
+data = json.load(file)
+
+sentences = []
+for line in data:
+    comment = line["r6"]
+    sentences.extend(sent_tokenize(comment))
+with open('../../data/sentences_r6.json', 'w') as outfile:
     json.dump(sentences, outfile)
 file.close()
